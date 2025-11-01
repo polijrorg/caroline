297c00e499073793c58199aaea4e8a17ea20a239
João Vitor de Aquino <jvaquinocontact@gmail.com>
2025-11-01 19:05:32 -0300

    feat: small ui changes, rename some stuff, change favicon

--- web/src/components/auth/LoginOptionals.tsx
+++ web/src/components/auth/LoginOptionals.tsx
@@ -3,13 +3,13 @@ import BaseCheckbox from "../input/BaseCheckbox/BaseCheckbox";
 
 function LoginOptionals() {
   return ( 
-    <div className="mt-4 flex items-center justify-between text-gray-500 text-sm">
+    <div className="mt-4 flex flex-col gap-2 justify-between text-gray-500 text-sm">
       <div className="flex items-center gap-2">
         <BaseCheckbox id="remember" name="remember"  />
         <label htmlFor="remember" className="leading-4">Lembrar de mim</label>
       </div>
 
-      <Link href='/login/reset-password' className="colorTransition border-transparent border-b hover:border-gray-400">Esqueceu sua senha?</Link>
+      <Link href='/login/reset-password' className="w-fit colorTransition border-transparent border-b hover:border-gray-400">Esqueceu sua senha?</Link>
     </div>
    );
 }
